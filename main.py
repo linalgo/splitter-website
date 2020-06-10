63ea678a47433230d4b2c61caca7115aa38aba34
Arnaud Rachez <arnaud.rachez@gmail.com>
2020-06-10 19:54:56 +0900

    code review

--- main.py
+++ main.py
@@ -1,70 +1,124 @@
-print("hello world")
-from scipy.stats import binom
-import numpy.random as rd
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
-data_size = 1000
-alpha = 3
-mu = 0
-sigma = 0.01
 
-beta = rd.uniform(-1, 1)
-x_values = numpy.zeros(data_size)
-for index in range(data_size):
-    x_values[index] = rd.uniform(0, 1)
+if __name__ == '__main__':
 
-error = rd.normal(mu,sigma)
-y = alpha * x_values + beta + error
-# y_float = alpha * x_values + beta + error
-# for idx in range(len(y_float)) :
-#     y_float[idx] = numpy.sign(y_float[idx])
-# y = y_float
-
-data_base = []
-for idx in range(data_size):
-    data_base.append((x_values[idx], y[idx]))
-#print(data_base)
-
-next_alpha = 0  # We start the search at x=0
-gamma = 1  # Step size multiplier
-precision = 0.000001  # Desired precision of result
-max_iters = 10000  # Maximum number of iterations
+    n = 1000
+    theta = np.array([0, 0])
+    x = np.random.uniform(0, 1, (n, 2))
+    x[:, 1] = 1
+    y = x.dot(theta) + np.random.normal(0, 0.2, n)
 
 
-def err_fun(alpha,beta,data_base,data_size):
-    sum = 0
-    for i in range(data_size):
-        iteration = (data_base[i][1]-alpha*data_base[i][0]-beta)**2
-        sum += iteration
-    return sum
+    def least_square_error(theta):
+        return sum((y - x.dot(theta))**2)
 
 
-# Derivative function
-def depsilon(alpha,beta,data_base,data_size):
-    sum = 0
-    for j in range(data_size) :
-        iteration = alpha*(data_base[j][0])**2 + data_base[j][0]*(beta - data_base[j][1])
-        sum += iteration
-    sum = 2 * sum
-    return sum / data_size
+    def least_square_error_derivative(theta):
+        return -2 * sum((y - x.dot(theta))[:, np.newaxis] * x)
+    
+    def sgd(theta0, n_iterations=100, step_size=0.01, precision=0.1,
+            verbose=False):
+        current_theta = theta0
+        for i in range(n_iterations):
+            derivative = least_square_error_derivative(current_theta)
+            next_theta = current_theta - step_size * derivative
+            if verbose:
+                print(f'iter #{i} - error = {least_square_error(next_theta)}')
+            step = next_theta - current_theta
+            if np.sqrt(step.dot(step)) < precision:
+                break
+            current_theta = next_theta
+        return current_theta
+    
+    theta_estimate = sgd(np.array([0, 1]), verbose=True)
+    print(f'{theta} VS {theta_estimate}')
 
 
-for _ in range(max_iters):
-    current_alpha = next_alpha
-    next_alpha = current_alpha - gamma * depsilon(current_alpha,beta,data_base,data_size)
+    # n = 1000
+    # alpha = 3
+    # mu = 0
+    # sigma = 0.01
 
-    step = next_alpha - current_alpha
-    if abs(step) <= precision:
-        break
+    # for _ in range(max_iters):
+    #     current_alpha = next_alpha
+    #     next_alpha = current_alpha - gamma * depsilon(current_alpha, beta, data_base, n)
 
-alpha_list = numpy.arange(0,6,0.01)
-error_list = []
-for k in alpha_list:
-    error_list.append(err_fun(k,beta,data_base,data_size))
+    #     step = next_alpha - current_alpha
+    #     if abs(step) <= precision:
+    #         break
 
 
-print("Minimum at ", next_alpha)
+    # def depsilon(alpha,beta,data_base,data_size):
+    #     sum = 0
+    #     for j in range(data_size) :
+    #         iteration = alpha*(data_base[j][0])**2 + data_base[j][0]*(beta - data_base[j][1])
+    #         sum += iteration
+    #     sum = 2 * sum
+    #     return sum / data_size
 
-plt.plot(alpha_list,error_list)
-plt.show()
+
+
+    # def err_fun(alpha, beta, data_base, data_size):
+    #     sum = 0
+    #     for i in range(data_size):
+    #         iteration = (data_base[i][1]-alpha*data_base[i][0]-beta)**2
+    #         sum += iteration
+    #     return sum
+
+
+    # error = np.random.normal(mu,sigma)
+    # y = alpha * x + beta + error
+    # # y_float = alpha * x_values + beta + error
+    # # for idx in range(len(y_float)) :
+    # #     y_float[idx] = np.sign(y_float[idx])
+    # # y = y_float
+
+    # data_base = []
+    # for idx in range(n):
+    #     data_base.append((x[idx], y[idx]))
+    # #print(data_base)
+
+    # next_alpha = 0  # We start the search at x=0
+    # gamma = 1  # Step size multiplier
+    # precision = 0.000001  # Desired precision of result
+    # max_iters = 10000  # Maximum number of iterations
+
+
+    # def err_fun(alpha, beta, data_base, data_size):
+    #     sum = 0
+    #     for i in range(data_size):
+    #         iteration = (data_base[i][1]-alpha*data_base[i][0]-beta)**2
+    #         sum += iteration
+    #     return sum
+
+
+    # # Derivative function
+    # def depsilon(alpha,beta,data_base,data_size):
+    #     sum = 0
+    #     for j in range(data_size) :
+    #         iteration = alpha*(data_base[j][0])**2 + data_base[j][0]*(beta - data_base[j][1])
+    #         sum += iteration
+    #     sum = 2 * sum
+    #     return sum / data_size
+
+
+    # for _ in range(max_iters):
+    #     current_alpha = next_alpha
+    #     next_alpha = current_alpha - gamma * depsilon(current_alpha, beta, data_base, n)
+
+    #     step = next_alpha - current_alpha
+    #     if abs(step) <= precision:
+    #         break
+
+    # alpha_list = np.arange(0,6,0.01)
+    # error_list = []
+    # for k in alpha_list:
+    #     error_list.append(err_fun(k, beta, data_base, n))
+
+
+    # print("Minimum at ", next_alpha)
+
+    # plt.plot(alpha_list,error_list)
+    # plt.show()
